2c16b6241b2240912cc3dc9eeab3d65f8bac1ba2
V-underEevee <136475712+V-underEevee@users.noreply.github.com>
2025-11-03 15:13:37 -0300

    sdfghjkl;

---
+++ src/Rol.java
@@ -0,0 +1,24 @@
+
+public enum Rol {
+
+
+	
+	Empleado(new String[] {"Dar de baja cajero","Reponer dinero"}),
+	Cliente(new String[] {"Transferir","Depositar","Retirar"});
+	
+	private String[] opciones;
+
+	private Rol(String[] opciones) {
+		this.opciones = opciones;
+	}
+
+	public String[] getOpciones() {
+		return opciones;
+	}
+
+	public void setOpciones(String[] opciones) {
+		this.opciones = opciones;
+	}
+	
+
+}
