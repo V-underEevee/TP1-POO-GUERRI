02a3eeeff8b6eeefe8927ab123c450da3229a5d8
V-underEevee <136475712+V-underEevee@users.noreply.github.com>
2025-11-04 13:13:47 -0300

    si

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,4 +1,28 @@
+import javax.swing.JOptionPane;
 
-public class Cliente {
+public class Cliente extends Usuario {
+private int saldo;
 
+public Cliente(String mail, String contr, Rol rol, int saldo) {
+	super(mail, contr, rol);
+	this.saldo = saldo;
+}
+
+public int getSaldo() {
+	return saldo;
+}
+
+public void setSaldo(int saldo) {
+	this.saldo = saldo;
+}
+
+@Override
+public String toString() {
+	return "Cliente [saldo=" + saldo + ", toString()=" + super.toString() + ", getMail()=" + getMail() + ", getContr()="
+			+ getContr() + ", getRol()=" + getRol() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + "]";
+}
+public void Menu() {
+
+	int opcion = JOptionPane.showOptionDialog(null, "Bienvenido cliente", "", 0, 0, null, this.getRol().getOpciones(),this.getRol().getOpciones()[0]);
+}
 }
--- src/Empleado.java
+++ src/Empleado.java
@@ -1,4 +1,11 @@
+import java.time.LocalDate;
 
-public class Empleado {
+public class Empleado extends Usuario {
+	
+	private LocalDate fechaInicio;
 
+	public Empleado(String mail, String contr, LocalDate fechaInicio) {
+		super(mail, contr, Rol.Empleado);
+		this.fechaInicio = fechaInicio;
+	}
 }
